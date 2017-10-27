575dbcc91121fc58fc213385649bccb010b0d8f6
Kathleen Nicholson <knichol8@nd.edu>
2017-10-27 13:44:04 -0400

    Added code for importing packages and added a description of the code for Q1

--- Exercise09_Answers.py
+++ Exercise09_Answers.py
@@ -1,3 +1,24 @@
 # Exercise 9 
 # Author: Grant Keller and Kathleen Nicholson
+# Import Packages ****I'm not yet sure if we need more or less of these.
 
+import numpy
+import pandas
+from scipy.optimize import minimize
+from scipy.stats import norm
+from plotnine import *
+
+
+# Q1
+## This code will perform a likelihood ratio test to determine which mutation 
+##      of three in the ponzr1 gene causes a significant reduction in mRNA levels
+
+
+
+
+#Q2
+
+
+
+
+#Q3
