6f64af39d23fe93f361551ab4f577eb1e31ed4b3
BaileeEgan <began3@nd.edu>
2017-10-27 14:45:08 -0400

    Coded for separate ttest nll for null model. Still doesn't work :(

--- exercise9.py
+++ exercise9.py
@@ -13,17 +13,29 @@ def nllike (p, obs):
     nll = -1 * scipy.stats.norm(expected, sigma).logpdf(obs.ponzr1Counts).sum()
     return nll
     
+def nllike_null (p, obs):
+    B0 = p[0]
+    sigma = p[1]
+    expected = B0
+    nll = -1 * scipy.stats.norm(expected, sigma).logpdf(obs.ponzr1Counts).sum()
+    return nll
+
 def ttest (data, group1, group2):
     initialGuess = np.array([1,1,1])
     temp_df = data[(data.mutation==group1) | (data.mutation==group2)]
     temp_df["x"] = 0
     temp_df["x"][temp_df.mutation == group2] = 1
-
-    fit1 = minimize(nllike, initialGuess, method = "Nelder-Mead", options={'disp': True}, args = temp_df[temp_df.mutation == group1])
-    fit2 = minimize(nllike, initialGuess, method = "Nelder-Mead", options={'disp': True}, args = temp_df[temp_df.mutation == group2])
     
-    D = fit1.x - fit2.x
+    # y = B0 + B1*x + E
+    model = minimize(nllike, [1, 1, 1], method = "Nelder-Mead", options={'disp': True}, args = temp_df)
+    
+    # y = B0 + E
+    null_model = minimize(nllike_null, [1, 1], method = "Nelder-Mead", options={'disp': True}, args = temp_df)
+    
+    
+    D = model.fun - null_model.fun
     print scipy.stats.chi2.cdf(x = (2 * D), df = 1)
 
-ttest(ponzr, "WT", "M124K")
+ttest(ponzr, "WT", "I213N")
+
 
