f603958a635abae1ee325bc732e806002c0988db
janicelove <jlove5@nd.edu>
2017-10-27 21:01:58 -0400

    added lists and lists -to-dataframe code

--- Janice_Q1.py
+++ Janice_Q1.py
@@ -36,12 +36,36 @@ pval = 1-chi2.cdf(x=2*(nllalt-nullnull), df = diffInNumParam)
 df = pd.read_csv("ponzr1.csv", delimiter= ',')
 
 WT = []
-M124K - []
+M124K = []
 I213N = [] 
 
-for i in df:
-    if mutation[i]=="WT":
-        WT.append()
-        
+#place gene counts into respective list: WT or specific mutation 
+for i in range(0,len(df),1):
+    if df.mutation[i]=="WT":
+        WT.append(df.ponzr1Counts[i])
+    elif df.mutation[i] == "M124K":
+        M124K.append(df.ponzr1Counts[i])
+    elif df.mutation[i] == "I213N":
+        I213N.append(df.ponzr1Counts[i])
+
+#create df with conditions to be compared 
+WT_df = pd.DataFrame({'WT':WT})
+WT_df['M124K']= M124K
+
+WT_I213N = pd.DataFrame({'WT':WT})
+WT_I213N['I213N']= I213N
+
+
+def nllike(p, obs):
+    B0 = p[0]
+    B1 = p[1]
+    sigma = p[2]
     
+    expected = B0 + B1*obs.x #make sure data import has a column called x 
     
+    nll=-1*norm(expected,sigma).logpdf(obs.y).sum()
+    return nll 
+    
+initialGuess=np.array([1,1,1])
+fit=minimize(nllike,initialGuess,method="Nelder-Mead",options={'disp': True},args=df)    
+
