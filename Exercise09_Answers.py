de59c0a75c317e1557c002be8785c4bc6729be22
Grant Keller <gkeller1@nd.edu>
2017-10-30 14:11:07 -0400

    Finished Q2, answers in good agreement with those given.

--- Exercise09_Answers.py
+++ Exercise09_Answers.py
@@ -8,9 +8,9 @@ from scipy.optimize import minimize
 from scipy.stats import norm
 from plotnine import *
 
-def nllike(p, obs):
-    expected = p[0] + p[1] * obs.x
-    return -1 * norm(expected, p[2]).logpdf(obs.y).sum()
+def nllike(p, obs, x, y):
+    expected = p[0] + p[1] * obs[x]
+    return -1 * norm(expected, p[2]).logpdf(obs[y]).sum()
 
 # Q1
 ## This code will perform a likelihood ratio test to determine which mutation 
@@ -45,16 +45,22 @@ This test can be accomplished with 1-scipy.stats.chi2.cdf(x=D,df=1) in Python.
 Use a likelihood ratio test to determine which of three mutations
 signiﬁcantly reduced the expression of ponzr1, a gene involved in
 the formation of the glomerulus in a developing zebraﬁsh kidney.
+
+ANSWERS
+
+M124K: p-value ~ 0.72 (no effect of treatment)
+V456D: p-value ~ 5.6e-6 (effect of treatment)
+I213N: p-value ~ 0.88 (no effect of treatment)
 """
 # this isn't correct, but it's a (working) start
 data = pandas.read_csv('ponzr1.csv')
-data['x'] = [0 for v in data.mutation]
+data['x1'] = [0 for v in data.mutation]
+data['x2'] = [['WT', 'M124K', 'V456D', 'I213N'].index(v) for v in data.mutation]
 data['y'] = data.ponzr1Counts
 initGuess=numpy.array([1,1,1])
-fit0 = minimize(nllike, initGuess, method="Nelder-Mead", options={'disp': True}, args=(data))
+fit0 = minimize(nllike, initGuess, method="Nelder-Mead", options={'disp': True}, args=(data,'x1','y'))
+fit1 = minimize(nllike, initGuess, method="Nelder-Mead", options={'disp': True}, args=(data,'x2','y'))
 D1 = nllike(fit0.x, data)
-data['x'] = [['WT', 'M124K', 'V456D', 'I213N'].index(v) for v in data.mutation]
-fit1 = minimize(nllike, initGuess, method="Nelder-Mead", options={'disp': True}, args=(data))
 D2 = nllike(fit1.x, data)
 
 diff = abs(D2 - D1)
@@ -67,30 +73,16 @@ diff = abs(D2 - D1)
 
 #Q2
 
+def nlllike_growth(p, obs, x, y):
+    expected = p[0] * obs[x] / (obs[x] + p[1])
+    return -1 * norm(expected, p[2]).logpdf(obs[y]).sum()
+
 growth_data = pandas.read_csv('MmarinumGrowth.csv')
+fit = minimize(nllike2, initGuess, method="Nelder-Mead", options={'disp': True}, args=(growth_data,'S','u'))
 
-a = ggplot(growth_data, aes(x='S', y='u'))
-
-equation = growth_data.S
-
-
-"""
-2. The maximum likelihood approach is very ﬂexible when choosing
-both the deterministic component and the probability distribution.
-
-For this problem we’ll stick with a normal distribution, but let’s
-try a non-linear model. Growth rates of bacteria in culture can be
-modeled as a function of the concentration of a limiting resource
-provided. Generally, these relationships have a saturating
-relationship between growth rate and resource supply known as the
-Monod Equation – µ = µmax S (S+Ks). Where µ is growth at a resource
-concentration S, µmax is the absolute maximum growth rate and Ks is
-the resource concentration at which growth is half of maximum.
-
-Given the data provided in “MmarinumGrowth.csv” estimate the maximum
-growth rate (µmax) and the half-saturation constant (Ks).
-"""
-
+print "The maximum growth rate is: {:.6}.".format(fit.x[0])
+print "The half maximal growth concentration is: {}.".format(int(round(fit.x[1])))
+print "Std. dev. (sigma) is: {:.1}.".format(fit.x[2])
 
 #Q3
 
@@ -127,4 +119,13 @@ the number of parameters between the two models.
 
 In the t-test example above, that will always be 1, but in this case
 it will be 1 or 2 depending on which models you are comparing.
+
+ANSWERS:
+Constant fit: B0 ~ 589.7, sigma ~ 164
+Linear fit: B0 ~ 318, B1 ~ 6.3, sigma ~ 54
+Quadratic fit: B0 ~ 180, B1 ~ 15.7, B2 ~ -0.11, sigma ~ 10.7
+The quadratic model is by for the best and the linear model is 
+a lot better than the constant or null model. The p-values for 
+the likelihood ratio tests should all be essentially zero (~1e-20 
+or less).
 """
