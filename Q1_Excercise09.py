363a032bbf3d1f3c61e4c63584b6964b10ebd0f0
Melissa Stephens <mpullins@nd.edu>
2017-10-30 21:50:02 -0400

    still working on the likelihood ratio test for Q1

--- Q1_Excercise09.py
+++ Q1_Excercise09.py
@@ -6,9 +6,10 @@ import numpy
 import pandas
 from scipy.optimize import minimize
 from scipy.stats import norm
+from scipy.stats import chi2
 from plotnine import *
 
-#need to populate dataframe with existing data
+#need to read-in/populate dataframe with existing data
 MyDataFrame=pandas.read_csv("ponzr1.csv",sep=",",header=0)
 
 #make 3 data frames with the control and treatment we are interested in. Initialize x=0
@@ -17,12 +18,23 @@ MyDataFrame=pandas.read_csv("ponzr1.csv",sep=",",header=0)
 Var1=MyDataFrame.loc[MyDataFrame.mutation.isin(["WT","M124K"]),:]
 Var2=pandas.DataFrame({'y':Var1.ponzr1Counts,'x':0})
 Var2.loc[Var1.mutation=="M124K","x"]=1
-print Var2
+
 Var3=MyDataFrame.loc[MyDataFrame.mutation.isin(["WT","V456D"]),:]
 Var4=pandas.DataFrame({'y':Var3.ponzr1Counts,'x':0})
 Var4.loc[Var3.mutation=="V456D","x"]=1
-print Var4
+
 Var5=MyDataFrame.loc[MyDataFrame.mutation.isin(["WT","I213N"]),:]
 Var6=pandas.DataFrame({'y':Var5.ponzr1Counts,'x':0})
 Var6.loc[Var5.mutation=="I213N","x"]=1
-print Var6
+
+def nllike(p,obs):
+	B0=p[0]
+	B1=p[1]
+	sigma=p[2]
+	expected=B0+B1*obs.x
+	nll=-1*norm(expected,sigma).logpdf(obs.y).sum()
+	return nll
+	
+#likelihood ratio test
+pval=1-chi2.cdf(x=2*(),df=1)
+print pval
